99388d2509a3e7b92f0677ab35b14400b87d09a0
SLEvelyn <nicolcrack@gmail.com>
2022-03-03 13:35:02 -0500

    Vuex Composition API

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,22 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    todos: [
+      { id: '1', text: 'Recolectar las piedras del infinito', completed: false },
+      { id: '2', text: 'Piedra del alma', completed: true },
+      { id: '3', text: 'Piedra de poder', completed: true },
+      { id: '4', text: 'Piedra de realidad', completed: false },
+      { id: '5', text: 'Conseguir nuevos secuaces competentes', compl2eted: false },
+    ]
   },
-  mutations: {
+  mtations: {
   },
   actions: {
+  },
+  getters: {
+    pendingTodos(state, getters, rootState) {
+      return state.todos.filter (todo => !todo.completed)
+    }
   },
   modules: {
   }
---
+++ src/views/TodoVuex.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>Lista de tareas de Thanos</h1>
+    <!-- <h4>Tareas: {{$store.state.todos.length}}</h4> -->
+    <h4>Pendientes: {{pending.length}}</h4>
+   
+
+</template>
+
+<script>
+import {useStore} from 'vuex'
+import {computed} from 'vue'
+
+export default {
+    setup() {
+
+        const  store = useStore() 
+        
+        return {
+            pending: computed(() => store.getters['pendingTodos'])
+        }
+
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
